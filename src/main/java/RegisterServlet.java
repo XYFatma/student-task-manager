2ed1b0de390f20eb774f3ff046d7515066576926
jenwils <jen.wils98@gmail.com>
2026-04-16 00:59:18 -0400

    feat: Added RegisterServlet.java
    Servlet to allow students to register with their name, email and
    password. Passwords are hashed using BCrypt.

---
+++ src/main/java/RegisterServlet.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("register.html");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        HttpSession session = request.getSession();
+
+        // Data validation
+        if (name == null || name.isBlank() ||
+                email == null || email.isBlank() ||
+                password == null || password.isBlank()) {
+
+                session.setAttribute("error", "fields");
+                response.sendRedirect("register.html");
+                return;
+            }
+        
+        StudentManager sm = new StudentManager();
+       
+        // Hashes password
+        String salt = BCrypt.gensalt(12);
+        String passwordHash = BCrypt.hashpw(password, salt);
+
+        // Saves user input
+        if (sm.createStudent(name.trim(), email.trim(), passwordHash)) {
+            session.setAttribute("success", "registered");
+            response.sendRedirect("login.html");
+        } else {
+            session.setAttribute("error", "failed");
+            response.sendRedirect("register.html");
+        }
+    }
+}
